9e595c0f3b1f0da40ac13db452428606311fa6dc
Zonyitoo <zonyitoo@gmail.com>
2013-02-08 18:40:53 +0800

    A typing mistake

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ else
     mkdir -p $SHARE/QDoubanFM
     cp doubanfm-qt $SHARE/QDoubanFM
     mkdir -p $SHARE/QDoubanFM/lang/
-    cp -r lang/*.pm $SHARE/QDoubanFM/lang/
+    cp -r lang/*.qm $SHARE/QDoubanFM/lang/
     rm -f $BIN/doubanfm-qt
     ln -s $SHARE/QDoubanFM/doubanfm-qt $BIN/doubanfm-qt
     cp QDoubanFM.desktop $APP
